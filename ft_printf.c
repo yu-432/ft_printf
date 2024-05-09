2e654ea908f5ce6a49ac545c6eef2938d5b8ffc2
yooshima <o.shimayu891@gmail.com>
2024-05-09 15:48:32 +0900

    remake ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,56 +6,54 @@
 /*   By: yooshima <yooshima@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 10:34:39 by yooshima          #+#    #+#             */
-/*   Updated: 2024/05/09 12:21:19 by yooshima         ###   ########.fr       */
+/*   Updated: 2024/05/09 15:48:04 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	if_per(const char *start, va_list ap, int count)
+int	if_per(const char *format, va_list ap, int count)
 {
-	if (*start == 'c')
+	if (*format == 'c')
 		count += put_c(va_arg(ap, int));
-	else if (*start == 's')
+	else if (*format == 's')
 		count += put_str(va_arg(ap, char *));
-	else if (*start == 'p')
+	else if (*format == 'p')
 		count += put_pointer((unsigned long long)va_arg(ap, void *));
-	else if (*start == 'd' || *start == 'i')
+	else if (*format == 'd' || *format == 'i')
 		count += put_nbr(va_arg(ap, int));
-	else if (*start == 'u')
+	else if (*format == 'u')
 		count += put_unsign_int(va_arg(ap, unsigned int));
-	else if (*start == 'x')
+	else if (*format == 'x')
 		count += put_hex(va_arg(ap, unsigned int), 0);
-	else if (*start == 'X')
+	else if (*format == 'X')
 		count += put_hex(va_arg(ap, unsigned int), 1);
-	else if (*start == '%')
+	else if (*format == '%')
 		count += write(1, "%", 1);
-	else if (*start != '\0')
-		count += write(1, start, 1);
+	else if (*format != '\0')
+		count += write(1, format, 1);
 	return (count);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list		ap;
-	const char	*start;
 	int			count;
 
 	count = 0;
 	va_start(ap, format);
-	start = format;
-	while (*start)
+	while (*format)
 	{
-		if (*start != '%')
-			count += put_c(*start);
-		else if (*start == '%')
+		if (*format != '%')
+			count += put_c(*format);
+		else if (*format == '%')
 		{
-			start++;
-			count = if_per(start, ap, count);
+			format++;
+			count = if_per(format, ap, count);
 		}
-		if (!*start)
+		if (!*format)
 			break ;
-		start++;
+		format++;
 	}
 	va_end(ap);
 	return (count);
